3c1932e1540e736c45c1622bbd9203f4a073ee7c
stef_fab@hotmail.it <stef_fab@hotmail.it>
2021-10-20 14:30:24 +0200

    add 1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 
-console.log ('stefano')
+// ottenere il nome
+// salvo in variabile
+// mostro output a schermo
+// selezione elemento html
+// assegnare valore all'elemento
+
+// name
+const nome = prompt ('Inserire il proprio nome');
+console.log ('nome');
+
+// cognome
+const cognome = prompt ('Inserire il proprio cognome');
+console.log ('cognome');
+
+// colore
+const colore = prompt ('Inserire il proprio colore preferito');
+console.log ('colore');
+
+
+// html + valore all'elemento
+document.getElementById ('result').innerHTML = `${nome} ${cognome} ${colore} 21`
